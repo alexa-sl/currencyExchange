005cc9790f313af5765b238fd3c1a088a044b6bc
alexa-sl <>
2024-01-12 14:39:44 +0300

    currencies list

--- src/app/components/currency-box/currency-box.component.ts
+++ src/app/components/currency-box/currency-box.component.ts
@@ -1,11 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {environment} from "../../../environments/environment.development";
+import currencies from "../../../assets/currencies.json";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-currency-box',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf
   ],
   templateUrl: './currency-box.component.html',
   styleUrl: './currency-box.component.sass'
@@ -14,15 +18,25 @@ export class CurrencyBoxComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   form: FormGroup;
+  currencies;
   ngOnInit() {
+    this.currencies = [...Object.values(currencies)];
     this.initForm();
   }
 
   initForm() {
     this.form = this.fb.group({
-      inputVal: new FormControl(null),
-      outputVal: new FormControl(null)
+      inputVal: new FormControl(1),
+      outputVal: new FormControl(null),
+      inputCur: new FormControl(this.currencies[92]), //RUB
+      outputCur: new FormControl(this.currencies[0]), //USD
     })
+  }
+
+  count(input: number, rate: number, control: string, currency) {
+    let result: number = input * rate;
+    console.log(currency?.code);
+    this.form.controls[control].setValue(result);
   }
 
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  api: "https://v6.exchangerate-api.com/v6/633cdaf58595c3cbb102660a/latest/"
+};
